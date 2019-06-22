1065b942e7339ceddfa142f4d0dc212fa4a8eb66
ProgWorker <fizikator@gmail.com>
2019-06-22 21:53:04 +0300

    added field to event_fragment

--- app/src/main/java/com/remember/app/ui/cabinet/memory_pages/events/current_event/CurrentEvent.java
+++ app/src/main/java/com/remember/app/ui/cabinet/memory_pages/events/current_event/CurrentEvent.java
@@ -79,8 +79,7 @@ public class CurrentEvent extends BaseActivity implements CurrentEventView {
     private void setItems(EventModel requestEvent){
         name.setText(requestEvent.getName());
         Glide.with(this)
-                .load(requestEvent.getPicture())
-                .apply(RequestOptions.circleCropTransform())
+                .load("http://86.57.172.88:8082"+requestEvent.getPicture())
                 .into(imageAvatar);
         dateView.setText(requestEvent.getDate());
         messageView.setText(requestEvent.getName() + " - " + requestEvent.getDescription());
