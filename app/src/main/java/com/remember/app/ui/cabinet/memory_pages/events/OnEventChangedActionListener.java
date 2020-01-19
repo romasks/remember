52cd7f2ef1552770cde4956cfbcc885f9df4e3b0
Dmytro <dmytro.bobyk@wise-engineering.com>
2020-01-19 13:29:19 +0200

    Fixed radio buttons for editing event and memory page

---
+++ app/src/main/java/com/remember/app/ui/cabinet/memory_pages/events/OnEventChangedActionListener.java
@@ -0,0 +1,8 @@
+package com.remember.app.ui.cabinet.memory_pages.events;
+
+import com.remember.app.data.models.EventModel;
+
+public interface OnEventChangedActionListener {
+
+    void onEventChanged(EventModel eventModel);
+}
