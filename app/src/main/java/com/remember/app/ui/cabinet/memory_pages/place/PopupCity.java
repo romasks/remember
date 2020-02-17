75eec548262c9cddaee9c06622c4a9258d06ac32
Raman Skaskevich <ras@novelpay.pl>
2020-02-17 14:58:15 +0100

    Fix theme on AddBurialPlace screen and PopupCity dialog

--- app/src/main/java/com/remember/app/ui/cabinet/memory_pages/place/PopupCity.java
+++ app/src/main/java/com/remember/app/ui/cabinet/memory_pages/place/PopupCity.java
@@ -5,14 +5,15 @@ import android.view.View;
 import android.widget.PopupWindow;
 import android.widget.SearchView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.remember.app.R;
 import com.remember.app.data.models.ResponseHandBook;
 import com.remember.app.ui.adapters.HandBookAdapter;
+import com.remember.app.ui.utils.Utils;
 
 import java.util.List;
-
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class PopupCity extends PopupWindow implements HandBookAdapter.Callback {
 
@@ -29,6 +30,9 @@ public class PopupCity extends PopupWindow implements HandBookAdapter.Callback {
         showAtLocation(contentView, Gravity.TOP, 0, 0);
 
         View popupView = getContentView();
+        if (Utils.isThemeDark()) {
+            popupView.findViewById(R.id.lay).setBackgroundColor(popupView.getContext().getResources().getColor(R.color.colorBlackDark));
+        }
         RecyclerView recyclerView = popupView.findViewById(R.id.rv_city);
         SearchView search = popupView.findViewById(R.id.search_city);
 
