eb0f131bfc7e77ac38d693fcca7929f2117a055d
claks <dmitry.kryvopust@gmail.com>
2020-11-05 14:13:46 +0200

    fix ui message

--- app/src/main/java/com/remember/app/ui/chat/message/adapter/ChatInputHolder.kt
+++ app/src/main/java/com/remember/app/ui/chat/message/adapter/ChatInputHolder.kt
@@ -3,6 +3,7 @@ package com.remember.app.ui.chat.message.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.remember.app.R
 import com.remember.app.data.models.ChatMessages
@@ -10,17 +11,30 @@ import com.remember.app.utils.KotlinUtils.parseTimeStampToTime
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.item_input_message.view.*
 
+
 class ChatInputHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
 
     fun onBind(item: ChatMessages.History, onLongClick: (position: Int, model: ChatMessages.History) -> Unit) {
         with(itemView) {
-           // isLongClickable = true
             tvMessage.text = item.content
-            tvTime.text = parseTimeStampToTime(item.date)
-//            setOnLongClickListener {
-//                onLongClick(adapterPosition, item)
-//                false
-//            }
+            if(item.content.length<30){
+                containerLine.visibility = View.VISIBLE
+                containerBottom.visibility = View.GONE
+                //containerBottom.maxWidth = 10
+                // containerLine.maxWidth = LinearLayout.LayoutParams.WRAP_CONTENT
+                tvTime.text = parseTimeStampToTime(item.date)
+            }
+            else{
+                containerLine.visibility = View.GONE
+                containerBottom.visibility = View.VISIBLE
+                // containerLine.maxWidth = 10
+                // containerBottom.maxWidth = LinearLayout.LayoutParams.WRAP_CONTENT
+                tvTimeBottom.text = parseTimeStampToTime(item.date)
+                with(tvMessage) {
+                    (layoutParams as ConstraintLayout.LayoutParams).setMargins(0, 0, 0, 65)
+                    requestLayout()
+                }
+            }
         }
     }
 
--- app/src/main/java/com/remember/app/ui/chat/message/adapter/ChatOutputHolder.kt
+++ app/src/main/java/com/remember/app/ui/chat/message/adapter/ChatOutputHolder.kt
@@ -3,13 +3,15 @@ package com.remember.app.ui.chat.message.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.remember.app.R
 import com.remember.app.data.models.ChatMessages
-import com.remember.app.utils.KotlinUtils.parseTimeStampToDateAndTime
 import com.remember.app.utils.KotlinUtils.parseTimeStampToTime
 import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_comments.view.*
 import kotlinx.android.synthetic.main.item_output_message.view.*
 
 
@@ -18,14 +20,46 @@ class ChatOutputHolder(override val containerView: View) :
     LayoutContainer {
 
     fun onBind(item: ChatMessages.History) {
-
         with(itemView) {
-            setOnClickListener {  }
             tvMessage.text = item.content
-            tvTime.text = parseTimeStampToTime(item.date)
+            if(item.content.length<30){
+                containerLine.visibility = View.VISIBLE
+                containerBottom.visibility = View.GONE
+                //containerBottom.maxWidth = 10
+               // containerLine.maxWidth = LinearLayout.LayoutParams.WRAP_CONTENT
+                tvTime.text = parseTimeStampToTime(item.date)
+            }
+            else{
+                containerLine.visibility = View.GONE
+                containerBottom.visibility = View.VISIBLE
+               // containerLine.maxWidth = 10
+               // containerBottom.maxWidth = LinearLayout.LayoutParams.WRAP_CONTENT
+                tvTimeBottom.text = parseTimeStampToTime(item.date)
+                with(tvMessage) {
+                    (layoutParams as ConstraintLayout.LayoutParams).setMargins(0, 0, 0, 65)
+                    requestLayout()
+                }
+            }
         }
     }
 
+
+    private fun showTimeLayout(itemView : View, item: ChatMessages.History){
+        with(itemView) {
+            tvMessage.text = item.content
+            if(item.content.length<15){
+                itemView.containerLine.visibility = View.VISIBLE
+                itemView.containerBottom.visibility = View.GONE
+                tvTime.text = parseTimeStampToTime(item.date)
+            }
+            else{
+                itemView.containerLine.visibility = View.GONE
+                itemView.containerBottom.visibility = View.VISIBLE
+                tvTimeBottom.text = parseTimeStampToTime(item.date)
+            }
+        }
+
+    }
     companion object {
         fun create(parent: ViewGroup): ChatOutputHolder =
             ChatOutputHolder(
